d0163950cc095f2e423e1a88a1d01cd5b5feda52
INDHUPRAKASH22122003 <indhuprakashtm@gmail.com>
2023-11-26 21:14:59 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-// src/App.js
 import './App.css';
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ProductForm from './components/ProductForm';
-import ProductList from './components/ProductList';
-
-
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './Login';
+import Product from './Product';
+import Register from './Register';
 function App() {
-  const [products, setProducts] = useState([
-    { id: 1, name: 'Product 1', description: 'Description 1', price: 10, quantity: 100, category: 'Category A' },
-    { id: 2, name: 'Product 2', description: 'Description 2', price: 20, quantity: 50, category: 'Category B' },
-  ]);
-
-  const addProduct = (product) => {
-    setProducts([...products, { ...product, id: products.length + 1 }]);
-  };
-
-  const editProduct = (editedProduct) => {
-    const updatedProducts = products.map((product) =>
-      product.id === editedProduct.id ? editedProduct : product
-    );
-    setProducts(updatedProducts);
-  };
-
-  const deleteProduct = (productId) => {
-    const updatedProducts = products.filter((product) => product.id !== productId);
-    setProducts(updatedProducts);
-  };
-
   return (
-    <div className="container">
-      <h1>Stock Management System</h1>
-      <ProductForm addProduct={addProduct} editProduct={editProduct} />
-      <ProductList products={products} editProduct={editProduct} deleteProduct={deleteProduct} />
+    <div>
+      {/* <Login/> */}
+      {/* <Register/> */}
+      <Product/>
     </div>
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/login" element={ <Login/> } />
+    //     <Route path="/register" element={ <Register/> } />
+    //     {/* <Route exact path="/" element={ <LandingPage/> } /> */}
+    //     {/* <Route path="/home" element={ <HomePage/> } /> */}
+    //   </Routes>
+    // </Router>
   );
 }
 
---
+++ src/Product.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductForm from './components/ProductForm';
+import ProductList from './components/ProductList';
+
+function Product(){
+    const [products, setProducts] = useState([
+        { id: 1, name: 'Product 1', description: 'Description 1', price: 10, quantity: 100, category: 'Category A' },
+        { id: 2, name: 'Product 2', description: 'Description 2', price: 20, quantity: 50, category: 'Category B' },
+      ]);
+    
+      const addProduct = (product) => {
+        setProducts([...products, { ...product, id: products.length + 1 }]);
+      };
+    
+      const editProduct = (editedProduct) => {
+        const updatedProducts = products.map((product) =>
+          product.id === editedProduct.id ? editedProduct : product
+        );
+        setProducts(updatedProducts);
+      };
+    
+      const deleteProduct = (productId) => {
+        const updatedProducts = products.filter((product) => product.id !== productId);
+        setProducts(updatedProducts);
+      };
+    
+      return (
+        <div className="container">
+          <h1>Stock Management System</h1>
+          <ProductForm addProduct={addProduct} editProduct={editProduct} />
+          <ProductList products={products} editProduct={editProduct} deleteProduct={deleteProduct} />
+        </div>
+      );
+    }
+export default Product;
